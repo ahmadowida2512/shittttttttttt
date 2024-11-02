d0884e6149c427e3d397c91a6967607fe96b96dd
ahmadowida2512 <aowida2512@gmail.com>
2024-11-02 11:00:48 +0200

    godddddddddd job

--- Assets/script/aaaa.cs
+++ Assets/script/aaaa.cs
@@ -4,50 +4,49 @@ using UnityEngine;
 
 public class aaaa : MonoBehaviour
 {
-    // Start is called before the first frame update
-      int Multiply(int a, int b)
-    {
-        return a * b;
-    }
-    void Start()
-    {
-    //     int playerLives =3;
-    //   while (playerLives >0)
-    //   {
-    //     if (playerLives == 3)Debug.Log("Many lives");
-    //   else if (playerLives == 1)Debug.Log("The last life");
-     
-    // playerLives--;
-    //   }
-            
-    //      Debug.Log("Game Over");
- int number=5;
-    string Summ=null;
-   
+   // Start is called before the first frame update
 
-   int i=1;
-        for ( ; i <= 10; i++)
-        {
-            Summ+=i+"x"+number+"="+(Sum(i,number));
-           
-    
- Summ+=",   ";
- Debug.Log(Sum(i,number));
-        }
-   Debug.Log(Summ);
+   void Start()
+   {
+      //     int playerLives =3;
+      //   while (playerLives >0)
+      //   {
+      //     if (playerLives == 3)Debug.Log("Many lives");
+      //   else if (playerLives == 1)Debug.Log("The last life");
 
-           
-          
-    }
+      // playerLives--;
+      //   }
 
- private int Sum(int num1,int num2){
-    return num1*num2;
-    
- }
+      //      Debug.Log("Game Over");
+      
+      int number = 5;
+      string Summ = null;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+      int i = 1;
+      for (; i <= 10; i++)
+      {
+         Summ += i + "x" + number + "=" + (Sum(i, number));
+
+
+         Summ += ",   ";
+         Debug.Log(Sum(i, number));
+      }
+      Debug.Log(Summ);
+
+
+
+   }
+
+   private int Sum(int num1, int num2)
+   {
+      return num1 * num2;
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
 }
